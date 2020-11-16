e00fad3f6d02d604a182e10d47072e5223c07ee5
Cécile FETRE <cecile.fetre@ens.uvsq.fr>
2020-11-16 14:25:02 +0100

    -

---
+++ l1-python-master/cours/Cours 9 nov.py
@@ -0,0 +1,26 @@
+# Exercice
+def tri(l):
+    l_triee = []
+    for i in range(len(l)):
+        minimum = min(l)
+        l_triee.append(minimum)
+        l.remove(minimum)
+    return l_triee
+
+
+tri([4, 13, 42, 27, 31, 17, 21])
+
+# Out[]: [4, 13, 17, 21, 27, 31, 42]
+
+# Exercice
+def compter_negatives(nombres):
+    """Renvoie le nombre d'éléments <0 de la liste passée en paramètre"""
+    compte = 0
+    for elem in nombres:
+        if(elem < 0):
+            compte += 1
+    return compte
+
+
+compter_negatives([3, -1, 1.5, 5, -6.3, -7.9, 2])
+# Out : 3
