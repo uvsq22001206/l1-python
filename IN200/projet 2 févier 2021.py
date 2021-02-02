800d9371ed55580c67b1b5239da3b856876db515
Cécile FETRE <cecile.fetre@ens.uvsq.fr>
2021-02-02 17:18:01 +0100

    jeu de la vie

--- IN200/projet 2 févier 2021.py
+++ IN200/projet 2 févier 2021.py
@@ -4,3 +4,35 @@
 #   - pour une case vivante : si 2 ou 3 voisins vivants, elle reste vivante
 #   - sinon la case meurt
 
+#############################
+# Auteurs :
+# FETRE Cécile
+# Groupe :
+# BI
+# URL
+#############################
+
+##########
+# Import des modules
+
+import tkinter as tk
+
+
+
+
+
+
+##########
+# Fonctions
+
+
+
+
+
+
+##########
+# Programme principal
+
+racine = tk.Tk()
+racine.title("Jeu de la vie")
+racine.mainloop()
